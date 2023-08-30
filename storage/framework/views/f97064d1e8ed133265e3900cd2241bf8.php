d9c7be4ace5f67e25700df90ea1f01f3438c5fb5
Emadeldeen Soliman <emade09@gmail.com>
2023-08-30 06:47:00 +0300

    Upgrade to livewire 3 and apply SPA

--- storage/framework/views/f97064d1e8ed133265e3900cd2241bf8.php
+++
@@ -1,5 +0,0 @@
-<div>
-     <?php $__env->slot('title', null, []); ?> <?php echo e(__('Home')); ?> <?php $__env->endSlot(); ?>
-     <?php $__env->slot('header', null, []); ?> <?php echo e(__('Home')); ?> <?php $__env->endSlot(); ?>
-</div>
-<?php /**PATH D:\wamp64\www\atmo-ndash\resources\views/livewire/dashboard/home.blade.php ENDPATH**/ ?>
